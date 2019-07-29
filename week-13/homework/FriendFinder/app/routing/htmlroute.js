feecf33efb833c983622e74155442a5f3bcf6535
rod080 <49286063+rod080@users.noreply.github.com>
2019-07-29 11:18:03 -0700

    installed packages

---
+++ week-13/homework/FriendFinder/app/routing/htmlroute.js
@@ -0,0 +1,80 @@
+/*
+questions to be answered 1 5
+read the followoing statements and choose wether tou agree with the statement. 
+
+1 strongly disagree
+2 disagreee
+3 indifferent 
+4 agreee 
+5 strongly agree
+
+
+
+1. I am dtf (for anything).
+
+2. I would like to have children.
+
+3. It is importnat to me that my match wants to have children.
+
+3. I rate myself a very attractive person. 
+
+4. My firends would rate me a very attractive person.
+
+5. tbh I'm just here to smash.
+
+6. I'm not gonna lie, I consider myself pretty thirsty right now.
+
+7. I want ot find my lifepaartner in the near future (couple years).
+
+8. I hope to find my life partner here.
+
+9. I able to admit my faults. I strive for a growth mentality.
+
+10. It is not easy for meto accept my faults. 
+
+
+\but their is problem with this approach
+
+
+two people can get a score close to each other but they could have ansered the questios completely different. a perosn could have arrived at their score, having answered the questions really different.
+
+tinder but with reviews on persn. incetiese guys to give a nice date
+
+determine the closests match by coming up with a difference equation. the values from the difference equation 
+
+people give advice but never take none
+
+people call you out on yourfaults but they never accept their wrongs 
+
+list of important values:
+humility 
+love 
+justice
+autonomy 
+individual freedom 
+
+
+money is a good way to measure success
+
+
+money is freedom 
+
+money is freedom 
+
+money is just a tool to accompish things. it does not provide a solution for all problesm. but it can hlep provide solutions fro many problems.
+
+I have a solid foundation from which my moral values originate
+
+the peopl of teh united statesare not better than any other group of people. however their values may be. 
+
+culture is simply the manifestation of the collected vlaues the individaul. 
+
+culture is the collection of people and the values they hold (or don't hold). 
+
+app that dsipkays the actions people have commited corredponig to the values they hold.
+
+*/
+
+
+
+
